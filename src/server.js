627bfd21979fd6e4a820024af2249d03514592c4
hayatshin <82467520+hayatshin@users.noreply.github.com>
2022-06-02 23:04:28 +0900

    introspection

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ const server = new ApolloServer({
   resolvers,
   typeDefs,
   playground: true,
+  introspection: true,
   context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token),
